be1960a7842b9e9e1a85bfe21c67f2a0f9f171d5
bilqisninsrina <bilqismulyadi07@gmail.com>
2026-08-16 00:58:59 +0700

    Move setup guide out of binary README

--- DN40/pendaftaran/tests.py
+++ DN40/pendaftaran/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RegistrationFlowTests(TestCase):
+    def test_home_is_public(self):
+        self.assertContains(self.client.get(reverse('home')), 'DIES')
+
+    def test_alumni_can_register_with_email(self):
+        response = self.client.post(reverse('login'), {'email': 'alumni@example.com', 'password': 'aman-sekali'}, follow=True)
+        self.assertRedirects(response, reverse('home'))
+        self.assertTrue(User.objects.filter(username='alumni@example.com').exists())
+
+    def test_buy_requires_login(self):
+        response = self.client.get(reverse('buy', args=['alumni']))
+        self.assertRedirects(response, f"{reverse('login')}?next={reverse('buy', args=['alumni'])}")
+
+    def test_sso_without_configuration_returns_guidance(self):
+        response = self.client.get(reverse('sso_start'), follow=True)
+        self.assertContains(response, 'SSO UI belum dikonfigurasi')
